ceb677e9f69bf6467a50934a676ddc70b33890c4
ntbloom <ntbloom@gmail.com>
2022-12-02 16:50:43 -0500

    [housekeeping] adding regression tests on real inputs

--- days/days_test.go
+++ days/days_test.go
@@ -3,6 +3,7 @@ package days_test
 import (
 	"fmt"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/ntbloom/aoc2022/parser"
@@ -27,6 +28,37 @@ func generateTest(day int, puzzle int, expected interface{}, t *testing.T) {
 	actual := s.Solve(puzzle)
 	if actual != expected {
 		t.Errorf("wanted %d, got %d", expected, actual)
+	}
+}
+
+func TestRegressions(t *testing.T) {
+	regressions := map[int][2]interface{}{
+		1: {71780, 212489},
+		2: {11150, 8295},
+	}
+	for day, solutions := range regressions {
+		for i, v := range []int{1, 2} {
+			filename, err := parser.GetFileName(day, parser.InputsDirectory)
+			filename = strings.Replace(filename, "days/", "", 1)
+			if err != nil {
+				panic(err)
+			}
+			fd := parser.GetFileDescriptor(filename)
+			defer func(fd *os.File) {
+				err = fd.Close()
+				if err != nil {
+					fmt.Printf("Unable to close test file descriptor %s\n", fd.Name())
+				}
+			}(fd)
+			s := solution.NewSolution(day, fd)
+
+			puzzle := v
+			expected := solutions[i]
+			actual := s.Solve(puzzle)
+			if actual != expected {
+				t.Errorf("day %d puzzle %d: wanted %d, got %d", day, puzzle, expected, actual)
+			}
+		}
 	}
 }
 
