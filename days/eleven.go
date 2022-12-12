ef0d5038896e6e3172b7c4f009d9e121a234f35c
ntbloom <ntbloom@gmail.com>
2022-12-11 22:45:01 -0500

    parsing mostly

--- days/eleven.go
+++ days/eleven.go
@@ -1,15 +1,22 @@
 package days
 
 import (
+	"bufio"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
+var itemId int = 0
+
 type Eleven struct {
-	fd *os.File
+	fd      *os.File
+	Monkeys map[int]*monkey
 }
 
 func CreateEleven(fd *os.File) *Eleven {
-	return &Eleven{fd}
+	return &Eleven{fd, map[int]*monkey{}}
 }
 
 func (eleven *Eleven) Solve(puzzle int) interface{} {
@@ -24,20 +31,82 @@ func (eleven *Eleven) Solve(puzzle int) interface{} {
 }
 
 func (eleven *Eleven) solve1() interface{} {
+	eleven.parse()
 	return nil
 }
 func (eleven *Eleven) solve2() interface{} {
 	return nil
 }
 
-func (eleven *Eleven) Parse() {
+func (eleven *Eleven) parse() {
+	scanner := bufio.NewScanner(eleven.fd)
+	var currentMonkey *monkey
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), " ")
+		if len(line) == 1 {
+			number := currentMonkey.Number
+			eleven.Monkeys[number] = currentMonkey
+			continue
+		}
+		if line[0] == "Monkey" {
+			number := getNumber(line[1])
+			currentMonkey = newMonkey(number)
+			continue
+		}
+		if line[2] == "Starting" {
+			for _, num := range line[4:] {
+				itm := newItem(getNumber(num))
+				currentMonkey.Items[itm.Id] = itm
+			}
+			continue
+		}
+	}
+	number := currentMonkey.Number
+	eleven.Monkeys[number] = currentMonkey
+	return
+	//for _, val := range line {
+	//	fmt.Print(val)
+	//	fmt.Print(" | ")
+	//}
+	//fmt.Print("\n")
+}
 
+type item struct {
+	Id         int
+	WorryLevel int
+}
+
+func newItem(worryLevel int) *item {
+	itm := item{Id: itemId, WorryLevel: worryLevel}
+	itemId++
+	return &itm
 }
 
 type monkey struct {
-	items       map[int]interface{}
-	operation   func(new, old int) int
-	criteria    func(value int) bool
-	trueMonkey  *monkey
-	falseMonkey *monkey
+	Number      int
+	Items       map[int]*item
+	Operation   func(old int) int
+	Criteria    func(value int) bool
+	TrueMonkey  int
+	FalseMonkey int
+}
+
+func newMonkey(number int) *monkey {
+	return &monkey{
+		Number:      number,
+		Items:       map[int]*item{},
+		Operation:   nil,
+		Criteria:    nil,
+		TrueMonkey:  -1,
+		FalseMonkey: -1,
+	}
+}
+
+func getNumber(str string) int {
+	numPattern := regexp.MustCompile(`\d+`)
+	if number, err := strconv.Atoi(numPattern.FindString(str)); err != nil {
+		panic(err)
+	} else {
+		return number
+	}
 }
