99078e9b6d4cc5eb9f227b40183ce3d4f1d5f8d3
ntbloom <ntbloom@gmail.com>
2022-12-12 22:00:27 -0500

    found path with breadth first search

--- days/twelve.go
+++ days/twelve.go
@@ -11,11 +11,11 @@ const Start uint8 = 83
 const End uint8 = 69
 
 type Twelve struct {
-	Elevations *[][]*grid
+	Elevations [][]*grid
 	Start      *grid
 	End        *grid
-	Width      int
-	Length     int
+	Width      uint8
+	Length     uint8
 	fd         *os.File
 }
 
@@ -23,8 +23,8 @@ func CreateTwelve(fd *os.File) *Twelve {
 
 	twelve := Twelve{
 		Start:  nil,
-		Width:  -1,
-		Length: -1,
+		Width:  0,
+		Length: 0,
 		fd:     fd,
 	}
 	twelve.parse()
@@ -43,7 +43,16 @@ func (twelve *Twelve) Solve(puzzle int) interface{} {
 }
 
 func (twelve *Twelve) solve1() interface{} {
-	return nil
+	d := bfs{
+		stack:  []*grid{},
+		twelve: twelve,
+	}
+	solution := d.find(twelve.Start)
+	if !solution.equal(twelve.End) {
+		panic("didn't find path!")
+	}
+	d.countToRoot(solution)
+	return d.Count
 }
 func (twelve *Twelve) solve2() interface{} {
 	return nil
@@ -55,15 +64,15 @@ func (twelve *Twelve) parse() {
 	rowCount := 0
 	for scanner.Scan() {
 		line := scanner.Text()
-		twelve.Width = len(line)
+		twelve.Width = uint8(len(line))
 		row := make([]*grid, twelve.Width)
 
 		for idx, val := range line {
 			height := uint8(val)
 			node := grid{
 				Height: height,
-				X:      uint8(rowCount),
-				Y:      uint8(idx),
+				Row:    uint8(rowCount),
+				Col:    uint8(idx),
 			}
 			if height == Start {
 				node.Height = Lowest
@@ -78,19 +87,86 @@ func (twelve *Twelve) parse() {
 		elevations = append(elevations, row)
 		rowCount++
 	}
-	twelve.Elevations = &elevations
-	twelve.Length = rowCount + 1
+	twelve.Elevations = elevations
+	twelve.Length = uint8(rowCount)
 }
 
 type grid struct {
-	X         uint8
-	Y         uint8
+	Row       uint8
+	Col       uint8
 	Height    uint8
 	Neighbors []*grid
 	Parent    *grid
+	Marked    bool
+}
+
+func (g *grid) equal(other *grid) bool {
+	return g.Row == other.Row && g.Col == other.Col
+}
+
+func (twelve *Twelve) findNeighbors(g *grid) {
+	var neighbors []*grid
+	// check left
+	if g.Col != 0 {
+		left := twelve.Elevations[g.Row][g.Col-1]
+		if (left.Height - g.Height) <= 1 {
+			neighbors = append(neighbors, left)
+		}
+	}
+
+	// check right
+	if g.Col != twelve.Width-1 {
+		right := twelve.Elevations[g.Row][g.Col+1]
+		if (right.Height - g.Height) <= 1 {
+			neighbors = append(neighbors, right)
+		}
+	}
+
+	// check up
+	if g.Row != 0 {
+		up := twelve.Elevations[g.Row-1][g.Col]
+		if (up.Height - g.Height) <= 1 {
+			neighbors = append(neighbors, up)
+		}
+	}
+
+	// check down
+	if g.Row != twelve.Length-1 {
+		down := twelve.Elevations[g.Row+1][g.Col]
+		if (down.Height - g.Height) <= 1 {
+			neighbors = append(neighbors, down)
+		}
+	}
+	g.Neighbors = neighbors
 }
 
 type bfs struct {
-	root  *grid
-	count int
+	stack  []*grid
+	twelve *Twelve
+	Count  int
+}
+
+func (b *bfs) find(g *grid) *grid {
+	g.Marked = true
+	if g.equal(b.twelve.End) {
+		return g
+	}
+	b.twelve.findNeighbors(g)
+	for _, val := range g.Neighbors {
+		if !val.Marked {
+			val.Parent = g
+			b.stack = append(b.stack, val)
+		}
+	}
+	next := b.stack[0]
+	b.stack = b.stack[1:]
+	return b.find(next)
+}
+
+func (b *bfs) countToRoot(g *grid) {
+	if g.equal(b.twelve.Start) {
+		return
+	}
+	b.Count++
+	b.countToRoot(g.Parent)
 }
